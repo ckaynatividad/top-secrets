78fcd720285fe42bb23230fb1a2bfb4334f6a5c1
ckaynatividad <natividad.ckay@gmail.com>
2022-03-29 17:04:31 -0700

    signin passing, added authtoken and passwordhash to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -18,5 +19,20 @@ describe('top-secrets routes', () => {
       .send({ username: 'omelette', password: 'hehe' });
 
     expect(res.body).toEqual({ id: expect.any(String), username: 'omelette' });
+  });
+
+  it('logs in user via post', async () => {
+    const user = await UserService.create({
+      username: 'omelette',
+      password: 'hehe',
+    });
+    const res = await request(app)
+      .post('/api/v1/auth/signin')
+      .send({ username: 'omelette', password: 'hehe' });
+
+    expect(res.body).toEqual({
+      message: 'youre in',
+      user,
+    });
   });
 });
